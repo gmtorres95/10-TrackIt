919c11aaf321f5a1a603b9d9fc0416febedee415
Gabriel Torres <al2012.163.torres@gmail.com>
2021-09-09 01:04:19 -0300

    Implemented SignUp page layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import "./assets/css/reset.css";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={SignIn} />
-                <Route exact path="/cadastro" />
+                <Route exact path="/cadastro" component={SignUp} />
                 <Route exact path="/habitos" />
                 <Route exact path="/hoje" />
                 <Route exact path="/historico" />
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 import logo from "../assets/img/logo.png";
 import { LoginBody, Input, Button } from "../components/styled/login";
 
-export default function SignIn() {
+export default function SignUp() {
     return (
         <LoginBody>
             <img src={logo} alt="TrackIt" />
             <Input placeholder="email" />
             <Input placeholder="senha" />
-            <Button>Entrar</Button>
-            <Link to="/cadastro">Não tem uma conta? Cadastre-se!</Link>
+            <Input placeholder="nome" />
+            <Input placeholder="foto" />
+            <Button>Cadastrar</Button>
+            <Link to="/">Já tem uma conta? Faça login!</Link>
         </LoginBody>
     );
 }
